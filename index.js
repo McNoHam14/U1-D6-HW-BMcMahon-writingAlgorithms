df5ba6ef5639b2b2569f4d73e935f9d5c4a88660
McNoHam14 <McNoHam14@gmail.com>
2022-11-28 13:26:06 +0000

    HW index

---
+++ index.js
@@ -0,0 +1,21 @@
+let fName = "Ben";
+let mName = "Michael";
+let sName = "McMahon";
+let age = 32;
+let currentYear = 2022;
+
+
+console.log(fName);
+console.log(mName);
+console.log(sName);
+console.log(age);
+
+let fullName = fName + " " + mName + " " + sName;
+
+console.log("Hello my name is", fullName);
+
+console.log("I am", age, "years old");
+
+let birthYear = currentYear - age;
+
+console.log("The Year of my Birth", birthYear);
